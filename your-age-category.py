e8af30b8fff6eac78f1300e9aade5a2a3d270ff7
unknown <m.r.khorram121@gmail.com>
2023-11-06 12:34:47 +0330

    with bag :(

--- your-age-category.py
+++ your-age-category.py
@@ -1,42 +1,37 @@
-# Program to determine your age category
-while True:
-    try:    
+# Program to determine your age category 
+# with bag :(
+
+age = int(input("Enter your age : "))
+
+while age :
+    if age <= 11:
+        print("You are a" , age, "years old child !")
         age = int(input("Enter your age : "))
         
-        while age :
-            if age <= 11:
-                print("You are a" , age, "years old child !")
-                age = int(input("Enter your age : "))
-                
-            elif (age >= 12 and age <= 18 ):
-                print("You are a" , age, "years old teenager !")
-                age = int(input("Enter your age : "))
-                
-            elif age == 19:
-                print("You are of legal age and You are a" , age, "years old teenager !")
-                age = int(input("Enter your age : "))
-            
-            elif age == 20:
-                print("You are a" , age, "years old teenager !")
-                age = int(input("Enter your age : "))        
-                
-            elif (age >= 21 and age <= 34):
-                print("You are a" , age, "years old young !")
-                age = int(input("Enter your age : "))
-                
-            elif (age >= 35 and age <= 49):
-                print("You are a" , age, "years old middle-aged !")
-                age = int(input("Enter your age : "))
-                
-            elif (age >= 50 and age <= 121):
-                print("You are a" , age, "years old man or woman !")
+    elif (age >= 12 and age <= 18 ):
+        print("You are a" , age, "years old teenager !")
+        age = int(input("Enter your age : "))
+        
+    elif age == 19:
+        print("You are of legal age and You are a" , age, "years old teenager !")
+        age = int(input("Enter your age : "))
     
-                age = int(input("Enter your age : "))
-                
-            else:
-                print("Enter the correct age")
-                age = int(input("Enter your age : "))
-                
-    except ValueError:
-            print("Please enter only numbers in the entry !")
-    continue
+    elif age == 20:
+        print("You are a" , age, "years old teenager !")
+        age = int(input("Enter your age : "))        
+        
+    elif (age >= 21 and age <= 34):
+        print("You are a" , age, "years old young !")
+        age = int(input("Enter your age : "))
+        
+    elif (age >= 35 and age <= 49):
+        print("You are a" , age, "years old middle-aged !")
+        age = int(input("Enter your age : "))
+        
+    elif (age >= 50 and age <= 121):
+        print("You are a" , age, "years old man or woman !")
+        age = int(input("Enter your age : "))
+        
+    else:
+        print("Enter the correct age")
+        age = int(input("Enter your age : "))
