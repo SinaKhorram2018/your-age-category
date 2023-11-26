ef7ce264ebbf9f42fc4090d00f4282fc938cd8ea
Sina Khorram <125433043+SinaKhorram2018@users.noreply.github.com>
2023-11-26 07:36:08 +0330

    Update your-age-category.py
     \n

--- your-age-category.py
+++ your-age-category.py
@@ -5,38 +5,38 @@ while True:
         
         while age :
             if age <= 11:
-                print("You are a" , age, "years old child !")
+                print("You are a" , age, "years old child ! \n")
                 age = int(input("Enter your age : "))
                 
             elif (age >= 12 and age <= 18 ):
-                print("You are a" , age, "years old teenager !")
+                print("You are a" , age, "years old teenager ! \n")
                 age = int(input("Enter your age : "))
                 
             elif age == 19:
-                print("You are of legal age and You are a" , age, "years old teenager !")
+                print("You are of legal age and You are a" , age, "years old teenager ! \n")
                 age = int(input("Enter your age : "))
             
             elif age == 20:
-                print("You are a" , age, "years old teenager !")
+                print("You are a" , age, "years old teenager ! \n")
                 age = int(input("Enter your age : "))        
                 
             elif (age >= 21 and age <= 34):
-                print("You are a" , age, "years old young !")
+                print("You are a" , age, "years old young ! \n")
                 age = int(input("Enter your age : "))
                 
             elif (age >= 35 and age <= 49):
-                print("You are a" , age, "years old middle-aged !")
+                print("You are a" , age, "years old middle-aged ! \n")
                 age = int(input("Enter your age : "))
                 
             elif (age >= 50 and age <= 121):
-                print("You are a" , age, "years old man or woman !")
+                print("You are a" , age, "years old man or woman ! \n")
     
                 age = int(input("Enter your age : "))
                 
             else:
-                print("Enter the correct age")
+                print("Enter the correct age ! \n")
                 age = int(input("Enter your age : "))
                 
     except ValueError:
-            print("Please enter only numbers in the entry !")
+            print("Please enter only numbers in the entry ! \n")
     continue
